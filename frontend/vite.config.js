b94a9cd0642495e596ca582bcc166aea999915d7
Delta student <139974922+PranchalTripathi@users.noreply.github.com>
2025-01-18 14:04:48 +0530

    resolving errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Socket } from 'socket.io-client'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build:{
+    rollupOptions:{
   plugins: [react()],
-})
+  external:['Socket.io-client'],
+},
+  },
+});
+
